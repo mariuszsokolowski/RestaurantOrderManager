9b697dec111290445a64c4c3ef244033328feb19
Mariusz Sokołowski <mariuszsokolowski95@gmail.com>
2024-10-05 21:22:57 +0200

    Add CLI feature for User seeds and show all commands

--- MyRestaurant.API/Startup.cs
+++ MyRestaurant.API/Startup.cs
@@ -1,54 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Owin;
-using Owin;
-using System.Web;
-using System.Web.Http;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Identity;
 using Swashbuckle.AspNetCore.Swagger;
-using System.Security.Claims;
-using Microsoft.AspNetCore.Http;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using MyRestaurant.Data.Entities;
-using MyRestaurant.API.Filters;
-using System.Reflection;
-using System.IO;
+using MyRestaurant.API.Enums;
 
 namespace MyRestaurant.API
 {
     public class Startup
     {
+        #region Fields
+        private string _command;
+        public IConfiguration Configuration { get; }
+        #endregion
+
+        #region Constructors
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
-
+            _command = Configuration.GetValue<string>("seed");
         }
+        #endregion
 
-
-
-        public IConfiguration Configuration { get; }
+        #region Methods
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-
             #region AddAutomMapper
             var config = new AutoMapper.MapperConfiguration(cfg =>
             {
@@ -59,7 +48,7 @@ namespace MyRestaurant.API
             services.AddSingleton(mapper);
 
             #endregion
-
+            #region Servvices
             services.AddMvc()
              .AddJsonOptions(options =>
                 {
@@ -83,54 +72,25 @@ namespace MyRestaurant.API
 
             });
 
-
-
-
-
-
-            /*services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
-            {
-                builder.AllowAnyOrigin()
-                       .AllowAnyMethod()
-                       .AllowAnyHeader();
-            }));*/
-
             services.AddCors(options => options.AddPolicy("MyPolicy",
          builder =>
          {
              builder.AllowAnyMethod().AllowAnyHeader()
-                    .WithOrigins("https://localhost:5001")
+                    .WithOrigins($"{Configuration.GetSection("CorsSettings:AllowedOrigins").Get<string[]>()}")
                     .AllowCredentials()
                     .AllowAnyHeader()
                     .AllowAnyMethod(); ;
          }));
 
-
-
-            // services.AddDbContext<Data.DBContext>(options => options.UseMySql(Configuration.GetConnectionString("MysqlConnection"), b => b.MigrationsAssembly("MyRestaruant.Data")));
             services.AddDbContext<Data.DBContext>(options => options.UseMySql(Configuration.GetConnectionString("MysqlConnection")));
-
-
-            /* services.AddIdentity<User, IdentityRole>()
-                     .AddRoleManager<RoleManager<IdentityRole>>()
-                     .AddDefaultUI()
-                     .AddDefaultTokenProviders()
-                     .AddEntityFrameworkStores<Data.DBContext>();*/
-
-            //services.AddDefaultIdentity<IdentityUser>().AddRoles<IdentityRole>();
-
 
             services.AddIdentity<User, Role>()
                              .AddDefaultUI()
                              .AddRoles<Role>()
                              .AddRoleManager<RoleManager<Role>>()
+                             .AddUserManager<UserManager<User>>()
                              .AddDefaultTokenProviders()
                              .AddEntityFrameworkStores<Data.DBContext>();
-
-
-            //services.AddScoped<IRoleValidator<IdentityRole>, RoleValidator<IdentityRole>>();
-            // services.AddScoped<RoleManager<IdentityRole>, RoleManager<IdentityRole>>();
-
 
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
             services
@@ -161,57 +121,13 @@ namespace MyRestaurant.API
                 options.AddPolicy("RequireAdministratorRole", policy => policy.RequireRole("Administrator"));
             });
             services.AddSignalR();
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-            /* var builder = services.AddIdentityServer(options =>
-             {
-                 options.IssuerUri = "MyIdentityServer";
-                 options.Events.RaiseErrorEvents = true;
-                 options.Events.RaiseInformationEvents = true;
-                 options.Events.RaiseFailureEvents = true;
-                 options.Events.RaiseSuccessEvents = true;
-             })
-                .AddTestUsers(new List<TestUser> {
-                     new TestUser{SubjectId = "1", Username = "zerokoll", Password = "test",
-                         Claims =
-                         {
-                             new Claim(JwtClaimTypes.Name, "Chris Klug"),
-                             new Claim(JwtClaimTypes.GivenName, "Chris"),
-                             new Claim(JwtClaimTypes.FamilyName, "Klug"),
-                             new Claim(JwtClaimTypes.Email, "chris@59north.com"),
-                         }
-                     }
-                });
-
-             // in-memory, json config
-             builder.AddInMemoryIdentityResources(Configuration.GetSection("IdentityResources"));
-             builder.AddInMemoryApiResources(Configuration.GetSection("ApiResources"));
-             builder.AddInMemoryClients(Configuration.GetSection("clients"));
-
-             if (Environment.IsDevelopment())
-             {
-                 builder.AddDeveloperSigningCredential();
-             }
-             else
-             {
-                 throw new Exception("need to configure key material");
-             }
-
-             AccountOptions.AllowRememberLogin = false;
-             AccountOptions.AutomaticRedirectAfterSignOut = true;
-             AccountOptions.ShowLogoutPrompt = false;
-             AccountOptions.WindowsAuthenticationEnabled = false;
-
-     */
-
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider _serviceProvider)
         {
-            //CreateRoles(_serviceProvider);
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -239,84 +155,118 @@ namespace MyRestaurant.API
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
-            CreateRoles(_serviceProvider);
 
-
-
-
+            if (!String.IsNullOrEmpty(_command))
+                SeedByCLI(_serviceProvider);
 
         }
 
-        /*public void ConfigureOAuth(IAppBuilder app)
-        {
 
-
-            //Identity
-            HttpConfiguration config = new HttpConfiguration();
-            WebApiConfig.Register(config);
-            app.UseWebApi(config);
-        }*/
-
+        #region Seeds
 
         /// <summary>
-        /// Używamy tylko podczas tworzenia nowych ról
+        /// Gets the list of available commands as a formatted string.
         /// </summary>
-        /// <param name="serviceProvider"></param>
-        /// <returns></returns>
-        private async Task CreateRoles(IServiceProvider serviceProvider)
+        /// <returns>Comma-separated list of available commands.</returns>
+        private List<string> GetAvailableCommands()
         {
-            //initializing custom roles 
-            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            return Enum.GetNames(typeof(SeedDataCommandEnums)).ToList();
+        }
 
-
-
-            string[] roleNames = { "Administrator", "Waiter", "Cook", "Client" };
-
-
-            foreach (var roleName in roleNames)
+        /// <summary>
+        /// Configures the CLI commands.
+        /// </summary>
+        /// <param name="app">The command line application instance.</param>
+        public void SeedByCLI(IServiceProvider serviceProvider)
+        {
+            try
             {
-                try
+                if (String.IsNullOrEmpty(_command))
                 {
-
-                    var roleExist = RoleManager.RoleExistsAsync(roleName);
-
-                    if (roleExist == null)
-                    {
-                        //create the roles and seed them to the database: Question 1
-                        var roleResult = RoleManager.CreateAsync(new IdentityRole(roleName));
-                        roleResult.Wait();
-                    }
+                    return;
                 }
-                catch (Exception e)
+
+                var commandList = GetAvailableCommands();
+                // Try to parse the command from the enum
+                if (_command == SeedDataCommandEnums.ShowCommands.ToString())
                 {
-                    var bug = e.Message;
+                    Console.WriteLine($"Available commands: {string.Join(", ", commandList)}");
+                }
+                else if (_command == SeedDataCommandEnums.SeedData.ToString())
+                {
+
+                }
+                else if (_command == SeedDataCommandEnums.SeedUsers.ToString())
+                {
+                    SeedUsers(serviceProvider);
+                }
+                else
+                {
+                    Console.WriteLine($"Unknown command: {_command}. Available commands: {string.Join(", ", commandList)}");
                 }
             }
-
-            //Here you could create a super user who will maintain the web app
-            var poweruser = new IdentityUser
+            catch (Exception e)
             {
-
-                UserName = Configuration["AppSettings:UserName"],
-                Email = Configuration["AppSettings:UserEmail"],
-            };
-            //Ensure you have these values in your appsettings.json file
-            string userPWD = Configuration["AppSettings:UserPassword"];
-            var _user = await UserManager.FindByEmailAsync(Configuration["AppSettings:AdminUserEmail"]);
-
-            if (_user == null)
-            {
-                var createPowerUser = await UserManager.CreateAsync(poweruser, userPWD);
-                if (createPowerUser.Succeeded)
-                {
-                    //here we tie the new user to the role
-                    await UserManager.AddToRoleAsync(poweruser, "Admin");
-
-                }
+                Console.WriteLine($"Error on using CLI command. {e.Message}");
             }
         }
 
+        private async void SeedUsers(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles 
+            try
+            {
+                var RoleManager = serviceProvider.GetRequiredService<RoleManager<Role>>();
+                var UserManager = serviceProvider.GetRequiredService<UserManager<User>>();
+                string[] roleNames = { "Administrator", "Waiter", "Cook", "Client" };
+                foreach (var roleName in roleNames)
+                {
+                    try
+                    {
+                        var roleExist = await RoleManager.RoleExistsAsync(roleName);
 
+                        if (roleExist == false)
+                        {
+                            //create the roles and seed them to the database: Question 1
+                            var roleResult = RoleManager.CreateAsync(new Role() { Name = roleName });
+                            roleResult.Wait();
+                        }
+                        else
+                        {
+                            Console.WriteLine($"The role already exists {roleName}");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error when created Role {e.Message}");
+                    }
+                }
+                //Here you could create a super user who will maintain the web app
+                var poweruser = new User
+                {
+                    UserName = Configuration["AdminSettings:UserName"],
+                    Email = Configuration["AdminSettings:UserEmail"],
+                };
+                //Ensure you have these values in your appsettings.json file
+                string userPWD = Configuration["AdminSettings:UserPassword"];
+                var _user = await UserManager.FindByEmailAsync(Configuration["AdminSettings:UserEmail"]);
+
+                if (_user == null)
+                {
+                    var createPowerUser = await UserManager.CreateAsync(poweruser, userPWD);
+                    if (createPowerUser.Succeeded)
+                    {
+                        await UserManager.AddToRoleAsync(poweruser, "Administrator");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        #endregion
+
+        #endregion
     }
 }
